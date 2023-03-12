5cc69fb1555154ca845f45c728c60d07de77ef50
Damzwan <damian.vlaicu@gmail.com>
2023-03-12 19:17:50 +0100

    added user name support
    fixed
    
    removed upload files
    
    unlink uploaded files
    
    git ignore upload documents

--- src/blob.ts
+++ src/blob.ts
@@ -6,40 +6,74 @@ import {
 } from '@azure/storage-blob';
 import { randomUUID } from 'crypto';
 
-enum CONTAINERS {
+export enum CONTAINERS {
   drawings = 'drawings',
+  account = 'account',
 }
 
 export class BlobCreator {
-  private static client: ContainerClient;
+  private static drawings: ContainerClient;
+  private static account: ContainerClient;
 
   constructor() {
     const account = process.env.blob_account;
     const accountKey = process.env.blob_key;
     if (account && accountKey) {
       const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
-      BlobCreator.client = new BlobServiceClient(
+      BlobCreator.drawings = new BlobServiceClient(
         `https://${account}.blob.core.windows.net`,
         sharedKeyCredential
       ).getContainerClient(CONTAINERS.drawings);
+
+      BlobCreator.account = new BlobServiceClient(
+        `https://${account}.blob.core.windows.net`,
+        sharedKeyCredential
+      ).getContainerClient(CONTAINERS.account);
       console.log('Connected to storage 🗑️');
     }
   }
 
-  async upload(content: string | Buffer, options?: BlockBlobUploadOptions) {
+  async upload(
+    content: string | Buffer,
+    options?: BlockBlobUploadOptions,
+    client = CONTAINERS.drawings
+  ) {
+    const blobClient =
+      client === CONTAINERS.drawings ? BlobCreator.drawings : BlobCreator.account;
     const blobName = randomUUID().toString();
-    const blockBlobClient = BlobCreator.client.getBlockBlobClient(blobName);
+    const blockBlobClient = blobClient.getBlockBlobClient(blobName);
     await blockBlobClient.upload(content, content.length, options);
     return blockBlobClient.url;
   }
 
-  async uploadImg(dataUrl: string) {
-    const base64Data = dataUrl.replace(/^data:image\/png;base64,/, ''); // Remove the data URL prefix
-    const buffer = Buffer.from(base64Data, 'base64');
+  async uploadImg(buffer: Buffer, client = CONTAINERS.drawings) {
     return await this.upload(buffer, {
       blobHTTPHeaders: {
         blobContentType: 'image/png',
       },
     });
+  }
+
+  async uploadFile(filePath: string, fileType: string) {
+    try {
+      const fileName = randomUUID().toString();
+      const blobClient = BlobCreator.account.getBlockBlobClient(fileName);
+      await blobClient.uploadFile(filePath, {
+        blobHTTPHeaders: {
+          blobContentType: fileType,
+        },
+      });
+      return blobClient.url;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async deleteBlob(blobUrl: string, container: CONTAINERS) {
+    const blobName = blobUrl.substring(blobUrl.lastIndexOf('/') + 1);
+    const client =
+      container === CONTAINERS.drawings ? BlobCreator.drawings : BlobCreator.account;
+    const blobClient = client.getBlobClient(blobName);
+    await blobClient.deleteIfExists();
   }
 }
