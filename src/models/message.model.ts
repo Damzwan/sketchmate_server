e38ff4d71e799cacf5d0a15c5ccb4118a2acd061
Damian <damian.vlaicu@gmail.com>
2026-05-11 21:40:21 +0200

    added big boy updates

--- src/models/message.model.ts
+++ src/models/message.model.ts
@@ -2,12 +2,15 @@ import mongoose, { Schema } from 'mongoose';
 import { AsDocument } from '../types/mongoose.types';
 import { BaseMessage } from '../types/types';
 
+const MessageSchema = new Schema({
+  conversation_id: { type: Schema.Types.ObjectId, ref: 'conversations', required: true },
+  sender_id: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+  content: { type: String, required: true },
+  is_invite: { type: Boolean, default: false }
+}, { timestamps: true });
+
+MessageSchema.index({ conversation_id: 1, createdAt: -1 });
 export const message_model = mongoose.model<AsDocument<BaseMessage, 'conversation_id' | 'sender_id'>>(
   'messages',
-  new Schema({
-    conversation_id: { type: Schema.Types.ObjectId, ref: 'conversations', required: true, index: true },
-    sender_id: { type: Schema.Types.ObjectId, ref: 'users', required: true },
-    content: { type: String, required: true },
-    is_invite: { type: Boolean, default: false },
-  }, { timestamps: true })
+  MessageSchema
 );
